87b6ac920449a55fe1afff54b0e8dce337b2dc54
Mirvari Mammadli <mirvari.mammadli@gmail.com>
2022-12-04 23:51:00 +0400

    done

--- script.js
+++ script.js
@@ -19,16 +19,20 @@ let weather = {
     displayWeather: function (data) {
       const { name } = data;
       const { icon, description } = data.weather[0];
-      const { temp, feels_like, humidity } = data.main;
+      const { temp, feels_like, temp_min, temp_max, humidity, pressure } = data.main;
       const { speed } = data.wind;
       document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
       document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".feels_like").innerText = feels_like + "°C";
+      document.querySelector(".feels_like").innerText = "Feels like: " + feels_like + "°C";
+      document.querySelector(".temp_min").innerText ="Minimum temperature: " + temp_min + "°C";
+      document.querySelector(".temp_max").innerText ="Maximum temperature" + temp_max + "°C";
       document.querySelector(".humidity").innerText =
         "Humidity: " + humidity + "%";
+        document.querySelector(".pressure").innerText =
+        "Pressure: " + pressure;
       document.querySelector(".wind").innerText =
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
