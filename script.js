8486a2a48abfe39c6a1a2a8c411f56219e3929a2
Mirvari Mammadli <mirvari.mammadli@gmail.com>
2022-12-04 21:59:23 +0400

    js

--- script.js
+++ script.js
@@ -16,9 +16,24 @@ let weather ={
 
       displayWeather: function(data){
         const {name}= data;
-        const {icon, description}= data.weather;
+        const {icon, description}= data.weather[0];
         const {temp, feels_like, temp_min, temp_max, humidty, pressure}=data.main;
         const {speed}= data.wind;
-        console.log(name, icon, description, temp, feels_like, temp_min, temp_max, humidty, pressure, speed )
+        console.log(name, icon, description, temp, feels_like, temp_min, temp_max, humidty, pressure, speed );
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src =
+          "https://openweathermap.org/img/wn/" + icon + "<a href="//www.flaticon.com/free-icons/cloud-computing" title="cloud computing icons">Cloud computing icons created by Freepik - Flaticon</a>";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp_min").innerText = temp_min + "°C";
+        document.querySelector(".temp_max").innerText = temp_max + "°C";
+        document.querySelector(".feels_like").innerText = feels_like + "°C";
+        document.querySelector(".humidity").innerText =  "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('nature.jpg" + name + "')";
+
       }
     };
